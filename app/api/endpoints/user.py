9d217030c233341de82e824f6e9596ea8f85e82e
mücahit keleş <mchtttkls@gmail.com>
2023-12-30 20:20:11 +0100

    getting user by id, username, email added in user_crud, endpoints are configured accordingly

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,4 +1,4 @@
-import hashlib
+from typing import List, Optional
 from mongoengine.errors import DoesNotExist
 from fastapi import APIRouter, HTTPException, status
 from jose import JWTError
@@ -7,7 +7,12 @@ from app.schemas.token import Token
 from app.crud import crud_user
 from app.utils import security, logger
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/users',
+    tags=['users'],
+    responses={404: {"description": "Not found"}}
+)
+
 logger = logger.setup_logger()
 
 
@@ -88,5 +93,87 @@ async def login(user: user_schema.UserLogin) -> Token:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Error processing login request."
+        )
+
+
+@router.get("/", response_model=List[user_schema.UserBase])
+async def read_users():
+    try:
+        users = crud_user.get_all_users()
+        return users
+    except DoesNotExist:
+        logger.warning(f"Users not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Users not found"
+        )
+    except JWTError as jwt_error:
+        logger.error(f"JWT Error: {jwt_error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error processing login request."
+        )
+    except Exception as e:
+        logger.error(f"Error occurred while processing getting users: {e}")
+
+
+@router.get("/{user_id}", response_model=user_schema.UserBase)
+async def read_user(user_id: int):
+    try:
+        user = crud_user.get_user_by_id(user_id=user_id)
+        return user
+    except DoesNotExist:
+        logger.warning(f"User not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    except Exception as e:
+        logger.error(f"Error occurred while processing getting user: {e}")
+
+
+@router.delete("/{user_id}", response_model=user_schema.UserBase)
+async def delete_user(user_id: int) -> Optional[user_schema.UserBase]:
+    """
+        Deletes a user from the database by their user ID.
+
+        Parameters:
+            user_id (int): The ID of the user to delete.
+
+        Returns:
+            Optional[user_schema.UserBase]: The deleted user's information if the operation was successful, None otherwise.
+
+        Raises:
+            HTTPException: With appropriate status code and detail message when errors occur.
+    """
+    try:
+        db_user = crud_user.get_user_by_id(user_id=user_id)
+        if not db_user:
+            logger.info(f"User with ID {user_id} not found")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found."
+            )
+
+        if crud_user.delete_user(user_id=user_id):
+            logger.info(f"User with ID {user_id} has been deleted.")
+            return db_user
+
+        else:
+            logger.warning(f"Failed to delete user with ID {user_id}. User may not exist.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found or already deleted."
+            )
+
+    except HTTPException as http_exc:
+        raise http_exc
+
+    except Exception as e:
+        logger.error(f"Unexpected error occurred while deleting user with ID {user_id}: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An unexpected error occurred while deleting the user."
         )
 
--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -1,7 +1,10 @@
 from app.schemas.user import UserCreate, UserBase
+from typing import List
 from app.models.user import User
 from app.utils import security, logger
-from mongoengine.errors import NotUniqueError, ValidationError
+from mongoengine.errors import NotUniqueError, ValidationError, DoesNotExist
+from fastapi import HTTPException, status
+
 logger = logger.setup_logger()
 
 
@@ -32,3 +35,93 @@ def create_user(user: UserCreate) -> UserBase | None:
 
     return None
 
+
+def get_all_users() -> List[UserBase]:
+    """
+    Retrieves all users from the database.
+    Returns a list of UserBase objects representing the users.
+    """
+    try:
+        users = User.objects.all()
+        return [UserBase(**user.to_mongo().to_dict()) for user in users]
+    except Exception as e:
+        logger.error(f"Error retrieving users: {e}")
+
+
+def get_user_by_username(username: str) -> UserBase | None:
+    try:
+        db_user = User.objects(username=username).first()
+        if db_user:
+            return UserBase(**db_user.to_mongo().to_dict())
+        else:
+            logger.info(f"User not found: {username}")
+            return None
+
+    except Exception as e:
+        logger.error(f"Error retrieving user by username {username}: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while retrieving the user."
+        )
+
+
+def get_user_by_id(user_id: int) -> UserBase | None:
+    try:
+        db_user = User.objects(id=user_id).first()
+        if db_user:
+            return UserBase(**db_user.to_mongo().to_dict())
+        else:
+            logger.info(f"User not found: user_id: {user_id}")
+            return None
+
+    except Exception as e:
+        logger.error(f"Error retrieving user by user_id: {user_id}: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while retrieving the user."
+        )
+
+
+def get_user_by_email(email: str) -> UserBase | None:
+    try:
+        db_user = User.objects(email=email).first()
+        if db_user:
+            return UserBase(**db_user.to_mongo().to_dict())
+        else:
+            logger.info(f"User not found: email: {email}")
+            return None
+    except Exception as e:
+        logger.error(f"Error retrieving user by user mail: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occured while retrieving the user."
+        )
+
+
+def delete_user(user_id: int) -> bool:
+    try:
+        db_user = User.objects(id==user_id).first()
+        if not db_user:
+            logger.info(f"User with ID {user_id} not found.")
+            return False
+        db_user.delete()
+        logger.info(f"User with ID {user_id} has been deleted.")
+        return True
+
+    except ValidationError as e:
+        logger.error(f"Validation Error: Invalid user ID format: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid user ID format."
+        )
+
+    except DoesNotExist:
+        logger.warning(f"User with ID {user_id} does not exist.")
+        return False
+
+    except Exception as e:
+        logger.error(f"Unexpected error occurred while deleting user with ID {user_id}.")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An unexpected error occurred while deleting the user."
+        )
